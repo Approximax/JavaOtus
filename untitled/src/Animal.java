b09ad2338c34cb9c9010e252bb895819d0901f4f
Approximax <vlad_pw_v@list.ru>
2022-05-31 18:07:44 +0300

    Первая итерация ДЗ по практике (15). Дальнейшие коммиты считать продолжением этой задачи, если не будет указано обратного или не вынесено в отдельный пакет.

---
+++ untitled/src/Animal.java
@@ -0,0 +1,44 @@
+public  class Animal {
+
+    public String name;
+    public int age;
+    public int weight;
+    public String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public void Say() {
+        System.out.println("Я говорю");
+    }
+}

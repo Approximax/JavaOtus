b545a40fff58e560b10c6f4a4a0532fe3fd897e2
Approximax <vlad_pw_v@list.ru>
2022-06-02 15:12:45 +0300

    Обновлен гитИгнор Реализован метод (велосипед) определения дописывания года, год, лет с генерацией ошибки, если возраст некорректен.

--- untitled/src/Animal.java
+++ untitled/src/Animal.java
@@ -1,9 +1,38 @@
-public  class Animal {
+public class Animal {
 
     public String name;
     public int age;
     public int weight;
     public String color;
+
+//    Как мне кажется тут получается некий "велосипед" по выборке лет/год/года, но пока что как лучше это сделать я ещё не придумал
+    public int last_year_number = age %10;
+
+    public String yearEnd(){
+        if ((age >=10) && (age <=20)) {
+            System.out.println("лет");
+        } else if ((last_year_number >=2) & (last_year_number <=4)) {
+            System.out.println("года");
+        } else if ((last_year_number >=5) & (last_year_number <=9)) {
+            System.out.println("лет");
+        }
+        throw new NullPointerException("Некорректно задан возраст.");
+    }
+
+    public void Say() {
+        System.out.println("Я говорю");
+    }
+    public void Go() {
+        System.out.println("Я иду");
+    }
+
+    public void Drink() {
+        System.out.println("Я пью");
+    }
+
+    public void Eat() {
+        System.out.println("Я ем");
+    }
 
     public String getName() {
         return name;
@@ -37,8 +66,8 @@ public  class Animal {
         this.color = color;
     }
 
-
-    public void Say() {
-        System.out.println("Я говорю");
+    @Override
+    public String toString() {
+        return "Привет! меня зовут" + name + ", мне" + age + yearEnd() + ", я вешу -" + weight + "кг, мой цвет - " + color;
     }
 }
--- untitled/src/Cat.java
+++ untitled/src/Cat.java
@@ -2,5 +2,5 @@ public class Cat extends Animal {
     @Override
     public void Say(){
         System.out.println("Мяу");
-    };
+    }
 }

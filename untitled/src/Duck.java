94cadf9b237fe04f041e2c0c882b59befc198f1b
Approximax <vlad_pw_v@list.ru>
2022-06-02 15:23:12 +0300

    Добавлена Собака и Утка с наследованием и реализацией интерфейса.

---
+++ untitled/src/Duck.java
@@ -0,0 +1,12 @@
+public class Duck extends Animal implements Flying{
+
+    @Override
+    public void Say() {
+        System.out.println("Кря");
+    }
+
+    @Override
+    public void Fly() {
+        System.out.println("Я лечу");
+    }
+}
